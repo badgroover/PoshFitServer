10d0bbc5f5ab4b92161f5425e242662c58aa77d9
badgroover <nikhil@poshmark.com>
2017-02-26 06:03:55 +0530

    Update header.js

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -25,16 +25,16 @@ $(document).ready(function(){
 	}
 
 	$(".navbar-nav #nav-yesterday").click(function(){
-		var href = $(this).attr("href"),
-			today = new Date(),
-			yesterday = new Date();
-
-		yesterday.setDate(today.getDate() - 1);
-		var year = yesterday.getFullYear(),
-			month = yesterday.getMonth() + 1,
-			day = yesterday.getDate();
-
-		$(this).attr("href", href + "?for=" + year + "-" + month + "-" + day);
+		var href = $(this).attr("href"), 
+			today = new Date(), 
+			year = today.getFullYear(),
+			month = today.getMonth() + 1,
+			day = today.getDate();
+		
+		var utc = today.toISOString();
+		var timezone = today.getTimezoneOffset();
+		
+		$(this).attr("href", href + "?for=" + year + "-" + month  + "-" + day + "&" + "utc=" + utc + "&timezone=" + timezone + "&yesterday=true");
 	});
 
 });
